b6a354f886f8df274bd46e8e62e26caa42293698
Piero <108848842+piero-uta@users.noreply.github.com>
2025-11-30 12:23:45 -0300

    publish app changes
    this commit includes the changes needed to publish to appstore and playstore

--- lib/environment.dart
+++ lib/environment.dart
@@ -2,9 +2,11 @@ class Urls {
   // Backend autolist
   // static String apiUrl = "https://delivera.autolistbeta.com";
   // Backend chifa
-  // static String apiUrl = "https://backend.chifaelmeson.cl";
+  static String apiUrl = "https://backend.chifaelmeson.cl";
 
   // Backend local
-  static String apiUrl = "http://10.0.2.2:8000";
+  // static String apiUrl = "http://10.0.2.2:8000";
+  // static String apiUrl = "http://localhost:8000";
+  // static String apiUrl = "http://192.168.1.12ß:8000";
   static int companyId = 1;
 }
